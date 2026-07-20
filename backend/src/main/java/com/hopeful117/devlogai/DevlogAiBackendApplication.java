dcfa34875abfff4033ad7d60433fc5f909c4ebd9
ludo <ludovic.brot@gmail.com>
2026-07-20 05:20:06 +0200

    initialize Spring Boot backend

---
+++ backend/src/main/java/com/hopeful117/devlogai/DevlogAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hopeful117.devlogai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevlogAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevlogAiBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hopeful117/devlogai/DevlogAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hopeful117.devlogai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlogAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
